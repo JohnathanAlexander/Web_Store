cca1e9b472aa746ef64931eb7365d8497082ec4f
Johnathan Alexander <johnathanalexander@aol.com>
2018-10-02 19:04:26 -0500

    Created common utility package.  Created Messages classes in utility package.

--- src/main/java/com/portfolio/onlineStore/controller/HomeController.java
+++ src/main/java/com/portfolio/onlineStore/controller/HomeController.java
@@ -1,21 +1,9 @@
 package com.portfolio.onlineStore.controller;
-
-
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.portfolio.onlineStore.entity.User;
-import com.portfolio.onlineStore.entity.Product;
 @Controller
 public class HomeController {
 	@RequestMapping("/")
